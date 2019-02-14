920b9724b22f699e6b14a8f6bdecb70ea96bc224
Zexdur <Zexdur@gmail.com>
2019-02-13 23:17:33 -0500

    Added basic HTML,CSS,and Javascript
    This is just adding basic HTML, CSS, and Javascript

---
+++ Sprint 1/index.js
@@ -0,0 +1,7 @@
+//This is called when the button is clicked
+function clickButton()
+{
+  //Add info to the div
+  var div = document.getElementById('divID');
+  div.innerHTML += '<p class = "description">We have used NYT font for the title, this is just to show how our server is up and running</p>';
+}
