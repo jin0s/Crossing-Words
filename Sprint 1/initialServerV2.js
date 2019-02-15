0bd0d8f750a978c782700d411df11feec9d5b929
Zexdur <Zexdur@gmail.com>
2019-02-14 20:45:46 -0500

    Update initialServerV2.js

--- Sprint 1/initialServerV2.js
+++ Sprint 1/initialServerV2.js
@@ -28,8 +28,9 @@ var server = http.createServer(function (request, response) {
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(htmlFile);
     }
-    });
-    response.end();
+        response.end();
+});
+
 
 
 //Listen on port 8000, IP defaults to 127.0.0.1
