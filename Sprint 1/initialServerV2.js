ed267b33ee9c6bee6af239d597585cf0d6a8d3a9
Zexdur <Zexdur@gmail.com>
2019-02-14 20:31:39 -0500

    Update initialServerV2.js

--- Sprint 1/initialServerV2.js
+++ Sprint 1/initialServerV2.js
@@ -1,19 +1,38 @@
-// Load the http module to create an http server.
 var http = require('http');
-var fs = require('fs');
 
-// Configure our HTTP server to respond with a glorious centaur with wings to all requests.
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
+var fs = require('fs');
+var htmlFile;
+var cssFile;
+
+fs.readFile('./index.html', function(err, data) {
+    if (err){
+        throw err;
+    }
+    htmlFile = data;
+});
+
+fs.readFile('./index.css', function(err, data) {
+    if (err){
+        throw err;
+    }
+    cssFile = data;
+});
+
+var server = http.createServer(function (request, response) {
+    switch (request.url) {
+        case "/AppClientStyle.css" :
+            response.writeHead(200, {"Content-Type": "text/css"});
+            response.write(cssFile);
+            break;
+        default :    
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(htmlFile);
     });
+    response.end();
 }
 
-http.createServer(onRequest).listen(8000);
+//Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+
+console.log("Server running at http://127.0.0.1:8000/");
