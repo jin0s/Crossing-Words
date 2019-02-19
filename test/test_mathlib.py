72900fe110087709dee9b5d2c6e08a155466a6f6
Jin Le <chinhle93@gmail.com>
2019-02-18 19:22:39 -0500

    initial test

---
+++ test/test_mathlib.py
@@ -0,0 +1,11 @@
+import mathlib
+
+
+def test_calc_total():
+    total = mathlib.calc_total(4,5)
+    assert total == 9
+
+
+def test_calc_multiply():
+    result = mathlib.calc_multiply(10,3)
+    assert result == 30
