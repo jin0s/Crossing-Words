68c603ac920861d9d2d03db2cb36cb6002d92499
sschilke <stevenschilke1@gmail.com>
2019-02-19 00:13:43 -0500

    Update test_generator1.py
    added the test to make sure an empty crossword was being correctly generated

--- Sprint 1/test/test_generator1.py
+++ Sprint 1/test/test_generator1.py
@@ -1,6 +1,25 @@
 from src import generator
 
-gen = generator.CrosswordGenerator(1, "../clues.csv", "./")
-
 def test_generate():
+    gen = generator.CrosswordGenerator(1, "clues.csv", "./")
     gen.generate()
+    try:
+        ex_filename = "empty_crossword.txt"
+        ex_file = open(ex_filename, "r")
+        ex_string = ex_file.read()
+        ex_file.close()
+        try:
+            gen_filename = "crossword0.txt"
+            gen_file = open(gen_filename, "r")
+            gen_string =  gen_file.read()
+            gen_file.close()
+            assert gen_string == ex_string
+        except FileNotFoundError:
+            print("File not generated")
+            raise
+        except AssertionError:
+            print("File incorrect")
+            raise
+    except FileNotFoundError:
+        print("Example file missing")
+        raise
