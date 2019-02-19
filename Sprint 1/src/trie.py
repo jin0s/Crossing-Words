b9d6188e7c4d929d81e0d854e57a62f2df078fdc
sschilke <stevenschilke1@gmail.com>
2019-02-18 23:56:41 -0500

    Update trie.py
    Made Trie class, which is a wrapper for the old Trie class, which is now the TrieNode class

--- Sprint 1/src/trie.py
+++ Sprint 1/src/trie.py
@@ -1,13 +1,24 @@
 class Trie:
     
     def __init__(self):
-        self.cur_char = '-'
+        self.head = TrieNode('-')
+        self.max_depth = 0
+        
+    def find_prefix(self, word):
+        return self.head.find_prefix(0, word)
+    
+    def insert_word(self, word):
+        self.head.insert_word(0, word)
+
+class TrieNode:
+    
+    def __init__(self, cur):
+        self.cur_char = cur
         self.next_char = [None]*26
         self.max_depth = 0
         
-    def find_prefix(index, word):
+    def find_prefix(self, index, word):
         return False
     
-    def insert_word(index, word):
+    def insert_word(self, index, word):
         return
- 
