21f2d5d163cdc67129f1fe00faace886d2653008
sschilke <stevenschilke1@gmail.com>
2019-02-18 20:39:40 -0500

    Create generator and trie

---
+++ Sprint 1/src/trie.py
@@ -0,0 +1,13 @@
+class Trie:
+    
+    def __init__(self):
+        self.cur_char = '-'
+        self.next_char = [None]*26
+        self.max_depth = 0
+        
+    def find_prefix(index, word):
+        return False
+    
+    def insert_word(index, word):
+        return
+ 
