bf47e2e4bbddf79d8cb24e437f3d92aed6b37deb
Zexdur <Zexdur@gmail.com>
2019-02-22 20:54:55 -0500

    Update index.js
    Created two new functions. One will take a JSON Object and turn it into data on the board. The other will take the x and y of a word and figure out which cell board that is a part of

--- Sprint 1/index.js
+++ Sprint 1/index.js
@@ -2,22 +2,73 @@
 var amountOfColumnsInGrid = 15;
 var amountOfRowsInGrid = 15;
 
+//THIS IS TEMP JUST FOR TESTING REASONS
+var tempJSONString = '{ "direction":"Down", "number":22, "x":3, "y":9, "answer":"NIGHT", "hint":"Dusk to dawn" }';
 
 //This function will draw the crossword
 function drawCrossword()
 {
   var html = '';
+  //This will be used to number each input from 1...255
+  var inputNumber = 1;
     //Loop through the amnount of amount of rows
-    for (var i = 0; i < amountOfRowsInGrid; i++)
+    for (var x = 0; x < amountOfRowsInGrid; x++)
     {
       //For every row loop through the amount of columns
-      for(var j = 0; j < amountOfColumnsInGrid; j++)
+      for(var y = 0; y < amountOfColumnsInGrid; y++)
       {
         //For every column write a block to the crossword
-        html += '<input class = "writeableBlock">';
+        html += '<input class = "writeableBlock" id = "'+inputNumber+'">';
+        inputNumber++;
       }
       //For every row we want to add a break line
       html += '<br>';
     }
     $('#CrosswordPuzzle').html(html);
+    addJSONDataToBoard();
+}
+
+
+
+//This function will take an x and y coordinate and find out what the id of that input is
+function turnXAndYToInputId(x,y)
+{
+  //Have a variable to hold the id that is going to be associated with this place
+  var id = 1;
+  //For every row that means that we must count each cell in that row
+  for(i = 0; i < x; i++)
+  {
+    id = id + amountOfRowsInGrid;
+  }
+  //Have to add all the cells that come before in this row
+  id = id + y;
+  //return the id of this input
+  return id;
+}
+
+//This function will take the JSON String and add the data to the field
+function addJSONDataToBoard()
+{
+  //Parse the JSON Data with all of the data of this word
+  var JSONData = JSON.parse(tempJSONString);
+  //Get the cell that this word starts on based on the x and y it starts of
+  var cellToWriteData = turnXAndYToInputId(JSONData.x, JSONData.y);
+  var placementOfWord = JSONData.direction;
+  var incrementation = 0;
+  //If the word is across we will add data increasing by columns
+  if(placementOfWord === "Across")
+  {
+    incrementation = 1;
+  }
+  //If the word is down we will add data increasing by rows
+  if(placementOfWord === "Down")
+  {
+    incrementation = amountOfColumnsInGrid;
+  }
+  //For every letter in the word we add a data type with the letter that is supposed to be there
+  for(var i = 0; i < JSONData.answer.length; i++)
+  {
+    $('#'+cellToWriteData).attr('data-letter', JSONData.answer.charAt(i));
+    cellToWriteData = cellToWriteData + incrementation;
+  }
 }
