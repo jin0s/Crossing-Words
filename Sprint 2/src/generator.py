22d1e0f37ae96c964511139bd23870c8d5de8531
sschilke <stevenschilke1@gmail.com>
2019-02-25 21:56:53 -0500

    Update generator.py
    Removed the old data loading code

--- Sprint 2/src/generator.py
+++ Sprint 2/src/generator.py
@@ -1,47 +1,34 @@
-from src import crossword as cw
-from src import trie
+import crossword as cw
+import clue_repository as cr
 import pandas as pd
 
 class CrosswordGenerator:
     global MAX_CROSSWORD_SIZE
     MAX_CROSSWORD_SIZE = 15
-    def __init__(self, clue_filename_, generate_filepath_):
+    
+    def __init__(self, _clue_filename, _generate_filepath):
         self.size = MAX_CROSSWORD_SIZE
-        self.clue_filename = clue_filename_
-        self.generate_filepath = generate_filepath_
-        self.clue_repo = None
+        self.generate_filepath = _generate_filepath
+        self.clue_repo = cr.ClueRepository(_clue_filename, MAX_CROSSWORD_SIZE)
         self.answers = None
         self.total_generated = 0
-
+        
     def generate(self):
-        if self.clue_repo is None:
-            self.load_clues()
-        if self.answers is None:
-            self.generate_trie()
+        if not clue_repo.is_loaded():
+            self.clue_repo.load()
         crossword = cw.Crossword(self.size)
         CrosswordGenerator.generate_structure(crossword)
         CrosswordGenerator.generate_clue_set(crossword)
         CrosswordGenerator.to_txt(crossword, self.generate_filepath, self.total_generated)
         self.total_generated += 1
-
+    
     def generate_structure(crossword):
-        return
-
+        return 
+    
     def generate_clue_set(crossword):
         return
-
-    def to_txt(crossword, filepath, n):
+    
+    def to_txt(crossword, filepath, n):    
         file = open(f'{filepath}\\crossword{n}.txt', "w+")
         file.write(str(crossword))
         file.close()
-
-    def load_clues(self):
-        self.clue_repo = pd.read_csv(self.clue_filename)
-        self.clue_repo.dropna()
-        self.generate_trie()
-
-    def generate_trie(self):
-        self.answers = trie.Trie()
-        unique = self.clue_repo.drop_duplicates('answer')['answer'].values
-        for word in unique:
-            self.answers.insert_word(word)
